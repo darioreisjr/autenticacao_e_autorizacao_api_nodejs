02414dd803014c4c5511ed259d096cd10f1973f5
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-24 04:52:54 -0300

    Feat: Adding Role to Requests

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -12,10 +12,11 @@ function ensureAuthenticated(request, response, next) {
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(token, authConfig.jwt.secret);
+    const { role, sub: user_id } = verify(token, authConfig.jwt.secret);
 
     request.user = {
       id: Number(user_id),
+      role
     };
 
     return next();
