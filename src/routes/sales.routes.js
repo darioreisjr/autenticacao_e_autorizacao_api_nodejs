967be0b6841c391c140160a914a9558818a1a3dc
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-24 05:20:56 -0300

    Feat: Applying Authorization to a Set of Routes

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const SalesController = require("../controllers/SalesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
 const salesRoutes = Router();
 
 const salesController = new SalesController();
 
 salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(verifyUserAuthorization("admin"));
 
 salesRoutes.get("/", salesController.index);
 
