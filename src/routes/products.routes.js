4acab8a8f92da286b68a32f06c9b8824297f7eb9
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-24 05:32:39 -0300

    Feat: Applying Authorization to Specific Routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const ProductsController = require("../controllers/ProductsController");
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
 
 const productsRoutes = Router();
 
@@ -9,6 +10,6 @@ const productsController = new ProductsController();
 productsRoutes.use(ensureAuthenticated);
 
 productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", productsController.create);
+productsRoutes.post("/", verifyUserAuthorization("admin"), productsController.create);
 
 module.exports = productsRoutes;
