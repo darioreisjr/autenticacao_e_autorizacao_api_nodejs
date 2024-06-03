10e0784b3b8688938a429d11ced35816a3bcabbe
Dario Reis <guttyerresjunior1995@gmail.com>
2024-06-03 05:31:03 -0300

    Feat:Validating User when logging into the application

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
+const UsersValidatedController = require("../controllers/UsersValidatedController");
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const UsersValidatedController = new UsersValidatedController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated , UsersValidatedController.index);
 
 module.exports = usersRoutes;
